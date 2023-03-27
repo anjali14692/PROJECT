9b017987b0a18df6c5c0525b3f226ff06095bb6b
anjali14692 <128732084+anjali14692@users.noreply.github.com>
2023-03-28 01:16:07 +0530

    code oprator

---
+++ LogicalOperator.java
@@ -0,0 +1,12 @@
+package DAY2;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		int a=100;
+		System.out.println(a>10 && a<20);
+		System.out.println(a>10 || a<20);
+		System.out.println(a!= 10);
+	}
+ 
+}
