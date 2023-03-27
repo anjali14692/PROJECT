19cd75f466a1746713bdceb7c59862da12883e95
anjali14692 <128732084+anjali14692@users.noreply.github.com>
2023-03-27 23:09:55 +0530

    JAVA CODE CALCULATION

---
+++ Calculation.java
@@ -0,0 +1,13 @@
+package DAY2;
+
+public class Calculation {
+
+	public static void main(String[] args) {
+		int a= 10;
+		int b= 20;
+		int c =a+b;
+		System.out.println("the sum is=" +c); 
+	}
+
+}
+;
